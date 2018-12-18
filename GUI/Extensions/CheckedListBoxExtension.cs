3085abb5a6806b363917f87f2ac9f20cd39e2cad
Michał Kiełbasa <michalkielbasa96@gmail.com>
2018-12-18 11:06:51 +0100

    Metody rozszerzające klasy TextBox i CheckedListBox.

---
+++ GUI/Extensions/CheckedListBoxExtension.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Windows.Forms
+{
+    public static class CheckedListBoxExtension
+    {
+        public static void ClearItemChecked(this CheckedListBox checkedListBox)
+        { 
+            while (checkedListBox.CheckedIndices.Count > 0)
+                checkedListBox.SetItemChecked(checkedListBox.CheckedIndices[0], false);
+        }
+    }
+}
---
+++ GUI/Extensions/TextBoxExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Forms
+{
+    public static class TextBoxExtension
+    {
+        public static string TextOrDefault(this TextBox textBox)
+        {
+            return string.IsNullOrWhiteSpace(textBox.Text) ? null : textBox.Text;
+        }
+    }
+}
