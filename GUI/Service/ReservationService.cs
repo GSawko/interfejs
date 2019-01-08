bd7ff45bf9eb0e45a5a412043704adb959d11c8a
Michał Kiełbasa <michalkielbasa96@gmail.com>
2019-01-08 04:32:45 +0100

    Dodanie klasy do obsługi rezerwacji.

---
+++ GUI/Service/ReservationService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Service
+{
+    static class ReservationService
+    {
+        public static List<REZERWACJE> GetAllNotTaken()
+        {
+            using (var entities = new DBEntities())
+            {
+                var reservation = entities.REZERWACJE
+                    .Where(r => r.PRACOWNICY_idPracWydaj == null)
+                    .Include("KLIENCI")
+                    .Include("POJAZDY")
+                    .ToList();
+
+                return reservation;
+            }
+        }
+
+        public static List<REZERWACJE> GetAllNotReturned()
+        {
+            using (var entities = new DBEntities())
+            {
+                var reservation = entities.REZERWACJE
+                    .Where(r => r.PRACOWNICY_idPracOdbier == null)
+                    .Include("KLIENCI")
+                    .Include("POJAZDY")
+                    .Include("PRACOWNICY")
+                    .ToList();
+
+                return reservation;
+            }
+        }
+    }
+}
