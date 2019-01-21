019100582cf37c6e4c4ae1c12f5c029cb8de18ea
Michał Kiełbasa <michalkielbasa96@gmail.com>
2019-01-21 02:00:16 +0100

    Metoda rozszerzająca dla klasy RichTextBox.

---
+++ GUI/Extensions/RichTextBoxExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Forms
+{
+    public static class RichTextBoxExtension
+    {
+        public static string TextOrDefault(this RichTextBox richTextBox)
+        {
+            return string.IsNullOrWhiteSpace(richTextBox.Text) ? null : richTextBox.Text;
+        }
+    }
+}
