864f8fef2b49250cfefb40a29deed220a8e6afe3
Michał Kiełbasa <michalkielbasa96@gmail.com>
2019-01-21 03:31:19 +0100

    Metoda rozszerzająca dla MaskedTextBox.

---
+++ GUI/Extensions/MaskedTextBoxExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Forms
+{
+    static class MaskedTextBoxExtension
+    {
+        public static string TextOrDefault(this MaskedTextBox maskedTextBox)
+        {
+            return string.IsNullOrWhiteSpace(maskedTextBox.Text) ? null : maskedTextBox.Text;
+        }
+    }
+}
