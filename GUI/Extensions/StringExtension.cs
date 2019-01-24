3a29641b8ef02ff4e252dc49e3be32eb28488919
Michał Kiełbasa <michalkielbasa96@gmail.com>
2019-01-24 23:04:17 +0100

    Roszerzenie o funkcję sprawdzającą czy string zawiera podany ciąg z możliwością uwzględnienia wielkości liter.

---
+++ GUI/Extensions/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        public static bool Contains(this String strings, string value, StringComparison stringComparison)
+        {
+            int index = strings.IndexOf(value, 0, stringComparison);
+
+            return index != -1 ? true : false;
+        }
+    }
+}
