e69afe1c5c81c6a2be85a11011b1958b2652ef5b
Michał Kiełbasa <michalkielbasa96@gmail.com>
2019-01-28 18:46:00 +0100

    Działająca konwersja zdjęć z bazy danych i odwrotnie.

--- GUI/Service/PhotoService.cs
+++ GUI/Service/PhotoService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,19 @@ namespace GUI.Service
 {
     public static class PhotoService
     {
+        public static byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
 
+        public static Image ByteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+
+            return returnImage;
+        }
     }
 }
